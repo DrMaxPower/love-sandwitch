49173ef00e95ab7634fecbc14e3ccae9e6787704
DrMaxPower <ni.fe@icloud.com>
2022-06-08 20:54:51 +0000

    input data validation runs to it is valid

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-# python code goes here
-import gspread 
+import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -22,25 +21,36 @@ SHEET = GSPREAD_CLIENT.open("love_sandwiches") # only smallcap allowed
 def get_sales_data():
     """ get sales figures from user """
 
-    print('Plz enter sales data from the last market day')
-    print('Data input in six numbers, separated by commas')
-    print("Example: 6,5,4,3,2,1\n")
+    while True:
+    
+        print('Plz enter sales data from the last market day')
+        print('Data input in six numbers, separated by commas')
+        print("Example: 6,5,4,3,2,1\n")
 
-    data_str = input('Enter your data here: ')
-    print(f'the data you send was {data_str}')
+        data_str = input('Enter your data here: ')
+        print(f'the data you send was {data_str}')
 
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
+        sales_data = data_str.split(',')
+        
+        if validate_data(sales_data):
+            print("data is valid")
+            break
 
 def validate_data(values):
     """ validate input data from get_sales_data """
     print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"expected six (6) number separated by comma (,) but {len(values) was given}"
+                f"expected six (6) number separated by comma (,) but {len(values)} was given"
             )
     except ValueError as e:
-        print(f"Invalid data: {e}, plz do it again, becouse, if tomorrow never comes \n")
+        print(f"Invalid data: {e} \n")
+        return False
+    
+    return True
 
-get_sales_data()
+
+data = get_sales_data()
+
