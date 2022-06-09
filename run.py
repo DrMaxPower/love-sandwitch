df35d3417415ddfd8ad40888ed61673e39d72baa
DrMaxPower <ni.fe@icloud.com>
2022-06-09 10:43:23 +0000

    building predictions functions of stocks inventory

--- run.py
+++ run.py
@@ -50,22 +50,12 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
-    """ Uppdate sales worksheet, add a new row of data """
-    print("updating sales woksheet...\n")
-    # reference the name of the worksheet in csv
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully. \n")
-
-
-def update_surplus_worksheet(data):
-    """ Uppdate surplus worksheet, add a new row of data """
-    print("updating surplus woksheet...\n")
-    # reference the name of the worksheet in csv
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("surplus worksheet updated successfully. \n")
+def update_worksheet(data, worksheet):
+    """ update google worksheet with sales, surplus & stocks """
+    print(f"updating {worksheet} worksheet... \n ")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -87,17 +77,28 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_enteries_sales():
+    """ stock calculator based on five last sales """
+
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
 def main():
     """ Run all program functions """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
-    
+    update_worksheet(new_surplus_data, "surplus")
     print(new_surplus_data)
 
 
 
 print("Welcome to love sandwiches data automation")
-main()
+# main()
+sales_columns = get_last_5_enteries_sales()
